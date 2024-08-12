4af9ede75b8baa2e0db986d760710c47c387debc
chuongdao <riochuong@gmail.com>
2024-08-12 06:55:40 +0000

    finish

--- gpt2_model.py
+++ gpt2_model.py
@@ -160,9 +160,9 @@ class GPT(nn.Module):
 
         # Create AdamW optimizer 
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device_type
         if master_process:
             print(f"using fused AdamW: {use_fused}")
-        use_fused = fused_available and 'cuda' in device_type
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
         return optimizer
 
